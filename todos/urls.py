82e9508651dc86e00789de465468a63ffbed8754
Anowar Hossen Farvez <farvez165@gmail.com>
2023-07-05 23:57:43 +0600

    path added for update and delete

--- todos/urls.py
+++ todos/urls.py
@@ -5,4 +5,7 @@ urlpatterns=[
     #path('',views.TodoListView),
     path('api/todos/', views.todos_list, name='todo-list'),
     path('', views.TodoListView.as_view(), name='todo-list'),
+    path('add/', views.addTodo, name='add-todo'),
+    path('todo/<int:pk>/delete/', views.delete_todo, name='delete-todo'),
+    path('todo/<int:pk>/update/', views.update_todo, name='update-todo'),
 ]
